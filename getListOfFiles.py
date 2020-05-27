c701e91724875049cae07d28586fb4e69238ea2e
Aymeric <aymeric.ferreira@gmail.com>
2020-05-27 17:05:24 -0400

    get List of files function
    Add a script to get all of the files in a folder and subfolders

---
+++ getListOfFiles.py
@@ -0,0 +1,21 @@
+import os
+
+def getListOfFiles(dirName):
+    """
+    Get a list of files in folder and subfolders of dirName
+
+	:param dirName: relative or absolute path to the directory
+	:return: List of files
+    """
+
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+
+    for entry in listOfFile:
+        fullPath = os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+
+    return allFiles
