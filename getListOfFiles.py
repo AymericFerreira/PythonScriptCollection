658253651c282349550c2476e79f2b5651be0ed9
Aymeric <aymeric.ferreira@gmail.com>
2020-06-26 12:21:19 -0400

    Correct the bad list creation

--- getListOfFiles.py
+++ getListOfFiles.py
@@ -3,18 +3,17 @@ import os
 def getListOfFiles(dirName):
     """
     Get a list of files in folder and subfolders of dirName
-
 	:param dirName: relative or absolute path to the directory
 	:return: List of files
     """
 
     listOfFile = os.listdir(dirName)
-    allFiles = list()
+    allFiles = []
 
     for entry in listOfFile:
         fullPath = os.path.join(dirName, entry)
         if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
+            allFiles += getListOfFiles(fullPath)
         else:
             allFiles.append(fullPath)
 
